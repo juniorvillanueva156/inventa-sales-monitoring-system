b4b68ac421358b66b0acee54cc4f3fe633567b25
juniorvillanueva156 <juniorvillanueva156@gmail.com>
2024-02-26 17:48:03 +0800

    First Commit: 2/26/2024

---
+++ src/main/java/com/cdfmsv/inventa/InventaApplication.java
@@ -0,0 +1,13 @@
+package com.cdfmsv.inventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdfmsv/inventa/InventaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdfmsv.inventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
